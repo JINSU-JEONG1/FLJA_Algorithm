2604904f5708edfd4bd3d94b4705ef992fb9323a
ohhyeonn <ohhyeon120@gmail.com>
2026-01-29 00:32:41 +0900

    서현오 커밋

--- HYEON-OH/DFS_BFS/감시피하기.py
+++ HYEON-OH/DFS_BFS/감시피하기.py
@@ -3,85 +3,78 @@
 # 설명:
 # range(start, stop, step)
 # stop은 포함 안 됨이라서 0까지 포함하려면 stop을 -1로 둬야 해.
+from itertools import combinations
 
-def isDirectionLog(x,y,direction):
-    for i in visited[x][y]:
-        if i == direction:
+def watch(map, x, y, n):
+    # 선생 (x,y)에서 학생이 보이면 True
+    # O 만나면 그 방향 중단
+    # X는 통과
+    # T는 통과해도 되지만 보통 상관없음(여기선 그냥 통과 처리)
+    # U
+    for i in range(x - 1, -1, -1):
+        if map[i][y] == "O":
+            break
+        if map[i][y] == "S":
             return True
-    
+    # D
+    for i in range(x + 1, n):
+        if map[i][y] == "O":
+            break
+        if map[i][y] == "S":
+            return True
+    # L
+    for j in range(y - 1, -1, -1):
+        if map[x][j] == "O":
+            break
+        if map[x][j] == "S":
+            return True
+    # R
+    for j in range(y + 1, n):
+        if map[x][j] == "O":
+            break
+        if map[x][j] == "S":
+            return True
+
     return False
-
-def searchWithDirection(map , x , y , n, visited):
-    # 오른쪽으로 먼저 탐색
-    for i in range(y+1 , n):
-        if map[x][i] == "T":
-            if isDirectionLog(x,i , "L") :
-                continue
-            else:
-                visited[x][i].append("L")
-                break
-
-    #왼쪽으로 탐색
-    for i in range(y-1 , -1 , -1):
-        if map[x][i] == "T":
-            if isDirectionLog(x,i , "R") :
-                continue
-            else:
-                visited[x][i].append("R")
-                break
-
-    #아래로 탐색
-    for i in range(x+1 ,n):
-        if map[i][y] == "T":
-            if isDirectionLog(i,y , "U") :
-                continue
-            else:
-                visited[i][y].append("U")
-                break
-
-    
-    # 위로 탐색        
-    for i in range(x-1 , -1 , -1):
-        if map[i][y] == "T":
-            if isDirectionLog(i,y , "D") :
-                continue
-            else:
-                visited[i][y].append("D")
-                break
-
-
-
-
-
-
 
 
 n = int(input())
-
-
-visited = [[ [] for _ in range(n) ] for _ in range(n) ]
-
-
-
+tPositions = []
+combiList = []
 map = []
 
 for i in range(n):
     map.append(input().split())
 
-
 for x in range(n):
     for y in range(n):
-         if map[x][y] == "S" :
-             searchWithDirection(map , x , y , n , visited)
-    
+        if map[x][y] == "T":
+            tPositions.append((x, y))
+        elif map[x][y] == "X":
+            combiList.append((x, y))
 
-count = 0
-for visitedRow in visited :
-    for visitedArray in visitedRow:
-        count += len(visitedArray)
+result = False
 
+for com in combinations(combiList, 3):
+    # 장애물 설치
+    for i in com:
+        map[i[0]][i[1]] = "O"
 
-if count <= 3:
+    bad = False
+    for t in tPositions:
+        if watch(map, t[0], t[1], n):
+            bad = True
+            break
+
+    # 원복
+    for i in com:
+        map[i[0]][i[1]] = "X"
+
+    if not bad:
+        result = True
+        break
+
+if result:
     print("YES")
 else:
     print("NO")
