ecdf647f0fe5d29dd946f9a3c10fb8293f0b3bf6
ohhyeonn <ohhyeon120@gmail.com>
2026-02-03 21:18:11 +0900

    ohhyeon commit

---
+++ HYEON-OH/Sort/실패율.py
@@ -0,0 +1,34 @@
+def solution(N, stages):
+    successCount = [0]*(N+2)
+    challengeCount = [0]*(N+2)
+    stuckCount = [0] * (N+2)
+    
+    for challenge in stages:
+        stuckCount[challenge] += 1
+    
+    
+    runningCount = stuckCount[N+1]
+    for i in range(N , 0 , -1):
+        runningCount += stuckCount[i]
+        challengeCount[i] = runningCount
+        successCount[i] = runningCount - stuckCount[i]
+    
+    
+    
+    probability = [0] * (N+1)
+    answer = []
+    
+    for stage in range(1 , N+1):
+        answer.append(stage)
+        if challengeCount[stage] == 0:
+            probability[stage] = 0
+            continue
+        
+        pro = (challengeCount[stage] - successCount[stage]) / challengeCount[stage]
+        probability[stage] = pro
+        
+    
+    answer.sort(key=lambda x : (-probability[x] , x))
+    
+    
+    return answer
