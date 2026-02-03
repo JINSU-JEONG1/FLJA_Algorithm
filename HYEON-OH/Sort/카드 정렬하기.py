55ef0b4abe70628e60adcf84176fedfd8f19cd33
ohhyeonn <ohhyeon120@gmail.com>
2026-02-03 22:46:58 +0900

    ohhyeon commit

--- HYEON-OH/Sort/카드 정렬하기.py
+++ HYEON-OH/Sort/카드 정렬하기.py
@@ -18,15 +18,11 @@ for i in range(n):
 
 result = 0
 
-if len(priorityQue) == 1:
+if len(priorityQue) == 1:              ## while len(priorityQue) != 0 : 여기를 1 아닐때로 헀으면 더깔끔하게 풀수있었음
     result = 0
-elif len(priorityQue) == 2:
-    num1 = heapq.heappop(priorityQue)
-    num2 = heapq.heappop(priorityQue)
-    result = num1 + num2
 else:
         
-    while len(priorityQue) != 0 :
+    while len(priorityQue) != 0 :       ## 여기를 0 아닐때 말고 1 아닐때로 했을때 더 깔끔하게 풀수있음
         if len(priorityQue) == 1:
             num = heapq.heappop(priorityQue)
             result += num
@@ -36,7 +32,7 @@ else:
 
         result += num1 + num2
 
-        if len(priorityQue) == 0:
+        if len(priorityQue) == 0:        ## 1 아닐때로 하고 풀면 조건 필요가 없어짐
             break
 
         heapq.heappush(priorityQue , num1 + num2)
